63f1c220e6794600824b47d352415549ee23545c
Zeranium <zenon9303@gmail.com>
2021-02-23 19:23:43 +0900

    2021/02/23 실습자료

---
+++ Ch06/lib/Account.py
@@ -0,0 +1,23 @@
+# 은행계좌 클래스
+
+class Account
+    # Attribute(변수)
+    bank = None
+    id = None
+    name = None
+    balance = 0
+
+    # Action(함수)
+    def deposit(self, cash):
+        self.balance += cash
+
+    def withdraw(self):
+        self.balance -= cash
+
+    def show(self):
+        print("은행명 :", self.bank)
+        print("계좌번호 :", self.id)
+        print("입금주 :", self.name)
+        print("잔액 :", self.balance)
+
+    pass
